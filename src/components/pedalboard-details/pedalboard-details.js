d869c17cf940a9ccf848be346e85aa8abbd684e0
Issoufi Adam <issoufi.adam1@gmail.com>
2018-11-12 18:00:44 +0100

    finalisation  de la page pedalboard-details-page

---
+++ src/components/pedalboard-details/pedalboard-details.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import "./pedalboard-details.scss";
+
+const PedalboardDetails = props => (
+  <div className="pedalboard-details-container">
+    <div className="pedalboard">
+      <header>
+        <div className="author">
+          <span className="author-name"> {props.author.name}</span>
+          <a href={props.uri} target="_blank" rel="noopener noreferrer">{props.uri}</a>
+        </div>
+        {props.pedalboardCount ?
+          <div className="count-container">
+            <a className="count" href="#null">
+              <span>{props.pedalboardCount}</span>
+            </a>
+            <span className="pedalboard-count-text">Pedalboards using it</span>
+          </div> :
+          null}
+      </header>
+      <h2>{props.label}</h2>
+      <div className="plugin-media-container">
+        <img className="plugin-media" src={props.screenshotUrl} alt="Plugin screenshot" />
+      </div>
+      <div className="category-container">
+        {
+          props.categories.map(categorie => <a href={`/plugins?category=${categorie}`} className="plugin-category">{categorie}</a>)
+        }
+      </div>
+      <p className="pedalboard-description" dangerouslySetInnerHTML={{ __html: props.description }} />
+
+      <div className="plugin-control-ports">
+        <table>
+          <thead>
+            <tr>
+              <th>Control</th>
+              <th>Default</th>
+              <th>Min</th>
+              <th>Max</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              props.controlPorts.map(controlPort => (
+                <tr>
+                  <td>{controlPort.name}</td>
+                  <td>{controlPort.default}</td>
+                  <td>{controlPort.min}</td>
+                  <td>{controlPort.max}</td>
+                </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+
+      <div className="plugin-misc-info-container">
+        <span className="plugin-uri">{props.uri}</span>
+        <span className="plugin-version">v{props.version}</span>
+      </div>
+    </div>
+  </div>
+);
+
+PedalboardDetails.propTypes = {
+  author: PropTypes.shape({
+    avatarUrl: PropTypes.string,
+    name: PropTypes.string
+  }),
+  brand: PropTypes.string,
+  categories: PropTypes.arrayOf(PropTypes.string),
+  controlPorts: PropTypes.arrayOf(PropTypes.shape({
+    default: PropTypes.number,
+    max: PropTypes.number,
+    min: PropTypes.number,
+    name: PropTypes.string
+  })),
+  description: PropTypes.string,
+  imageHeight: PropTypes.number,
+  imageWidth: PropTypes.number,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  pedalboardCount: PropTypes.number,
+  screenshotUrl: PropTypes.string,
+  stable: PropTypes.bool,
+  thumbnailUrl: PropTypes.string,
+  uri: PropTypes.string
+}
+
+export default PedalboardDetails;
